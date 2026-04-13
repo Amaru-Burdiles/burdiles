0a6b845ddbeefa97db4fdf64a1af9377b8a8ed16
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-13 10:41:27 -0400

    Proyecto base y port config

---
+++ burdiles/src/main/java/duoc/amaru/burdiles/BurdilesApplication.java
@@ -0,0 +1,13 @@
+package duoc.amaru.burdiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurdilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurdilesApplication.class, args);
+	}
+
+}
